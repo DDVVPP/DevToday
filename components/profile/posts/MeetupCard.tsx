f027c706715ccb2fac3b8b4049451f316ea9a11c
DDVVPP <darshinvp@gmail.com>
2024-07-08 19:06:29 -0700

    style: updated hover state for meetupcard

--- components/profile/posts/MeetupCard.tsx
+++ components/profile/posts/MeetupCard.tsx
@@ -13,37 +13,36 @@ interface MeetupCardProps {
 
 const MeetupCard = ({ meetup, index = 1 }: MeetupCardProps) => {
   return (
-    <>
-      {meetup && (
-        <MotionDiv
-          initial={{
-            opacity: 0,
-
-            x: index % 2 === 0 ? -50 : 50,
-          }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.5 }}
-          className="flex flex-col space-y-[10px] rounded-[16px] bg-white-100 p-5 dark:bg-dark-800"
-        >
-          <div className="flex flex-col gap-y-4">
+    meetup && (
+      <MotionDiv
+        initial={{
+          opacity: 0,
+          x: index % 2 === 0 ? -50 : 50,
+        }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <div className="group flex cursor-pointer flex-col rounded-[16px] bg-white-100 p-5 transition duration-300 hover:bg-[#e1e2e6] dark:bg-dark-800 dark:hover:bg-dark-700">
+          <Link
+            href={`/meetups/${meetup.id}`}
+            className=" flex flex-col gap-y-4"
+          >
             <div className="flex items-start justify-between">
               <div className="flex items-center gap-x-5">
                 <MotionDiv whileHover={{ scale: 1.1 }}>
-                  <Link href={`/meetups/${meetup.id}`}>
-                    <Image
-                      src={meetup?.image!}
-                      width={72}
-                      height={72}
-                      alt="meetup-image"
-                      className="size-[72px] shrink-0 self-start rounded-[6px] max-lg:hidden"
-                    />
-                  </Link>
+                  <Image
+                    src={meetup?.image!}
+                    width={72}
+                    height={72}
+                    alt="meetup-image"
+                    className="size-[72px] shrink-0 self-start rounded-[6px] max-lg:hidden"
+                  />
                 </MotionDiv>
                 <div className="flex flex-col gap-y-2">
                   <h3 className="paragraph-1-bold max-lg:paragraph-3-bold text-dark-800 max-lg:line-clamp-1 dark:text-white-200">
                     {meetup?.title}
                   </h3>
-                  <p className="paragraph-3-regular text-white-400 max-lg:line-clamp-1">
+                  <p className="paragraph-3-regular text-white-400 transition duration-300 group-hover:text-dark-700 max-lg:line-clamp-1 dark:group-hover:text-white-300">
                     {meetup?.address}
                   </p>
                 </div>
@@ -52,7 +51,7 @@ const MeetupCard = ({ meetup, index = 1 }: MeetupCardProps) => {
             </div>
             <div
               dangerouslySetInnerHTML={{ __html: meetup?.body! }}
-              className="paragraph-3-regular text-white-400 dark:text-white-200"
+              className="paragraph-3-regular text-white-400 group-hover:text-dark-700 dark:text-white-200 dark:group-hover:text-white-100"
             ></div>
             <div className="flex justify-between">
               <div className="flex gap-x-2.5">
@@ -65,10 +64,10 @@ const MeetupCard = ({ meetup, index = 1 }: MeetupCardProps) => {
                 contentId={meetup.id!}
               /> */}
             </div>
-          </div>
-        </MotionDiv>
-      )}
-    </>
+          </Link>
+        </div>
+      </MotionDiv>
+    )
   );
 };
 
