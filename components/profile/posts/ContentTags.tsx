824ab8e6fce8ccda63e8095456a274e650dd7c34
DDVVPP <darshinvp@gmail.com>
2024-07-08 17:54:03 -0700

    update: adjusted hover state for postCard and children

--- components/profile/posts/ContentTags.tsx
+++ components/profile/posts/ContentTags.tsx
@@ -5,7 +5,7 @@ const ContentTags = ({ tag }: { tag: string }) => {
   return (
     <div className="my-2 lg:ml-3">
       <Button
-        className={`paragraph-3-regular caption-10 h-5  cursor-default gap-x-2.5 rounded-[20px] bg-white-200 uppercase text-white-400  dark:bg-dark-700 dark:text-white-300`}
+        className={`paragraph-3-regular caption-10 h-5  cursor-default gap-x-2.5 rounded-[20px] bg-white-200 uppercase text-white-400  transition duration-300 dark:bg-dark-700 dark:text-white-300 dark:group-hover:bg-dark-800`}
       >
         {tag}
       </Button>
