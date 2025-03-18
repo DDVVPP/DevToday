34d8337e9aa8cc6c7a5e7fd571037fa2e807369a
DDVVPP <darshinvp@gmail.com>
2025-03-18 09:55:23 -0700

    wip: added no content text to ContentNotFound component for Profile component

--- components/shared/ContentNotFound.tsx
+++ components/shared/ContentNotFound.tsx
@@ -3,18 +3,42 @@ import Link from "next/link";
 
 const ContentNotFound = ({
   contentCategory,
+  isPlural,
 }: {
   contentCategory: ContentCategoryType;
+  isPlural?: boolean;
 }) => {
   const pluralLowerCaseContentType = `${contentCategory.toLowerCase()}s`;
 
   return (
     <div className="paragraph-1-medium m-20 flex flex-col justify-center gap-y-6 text-dark-700 dark:text-white-200">
-      <h1 className="text-center">{contentCategory} not found</h1>
-      <Link
-        className="rounded bg-primary-500 px-4 py-2 text-white-200 hover:opacity-70"
-        href={`/${pluralLowerCaseContentType}`}
-      >{`Click here to view all ${pluralLowerCaseContentType}`}</Link>
+      {isPlural ? (
+        <>
+          <h1 className="text-center">{contentCategory}s not found</h1>
+          <Link
+            className="rounded bg-primary-500 px-4 py-2 text-white-200 hover:opacity-70 hover:duration-300"
+            href={`/${pluralLowerCaseContentType}`}
+          >
+            Click here to view all {pluralLowerCaseContentType}
+          </Link>
+          <Link
+            className="rounded bg-primary-500 px-4 py-2 text-center text-white-200 hover:opacity-70 hover:duration-300"
+            href={`/${pluralLowerCaseContentType}/create-post`}
+          >
+            Click here to create {contentCategory.toLowerCase()}
+          </Link>
+        </>
+      ) : (
+        <>
+          <h1 className="text-center">{contentCategory} not found</h1>
+          <Link
+            className="rounded bg-primary-500 px-4 py-2 text-white-200 hover:opacity-70 hover:duration-300"
+            href={`/${pluralLowerCaseContentType}`}
+          >
+            Click here to view all {pluralLowerCaseContentType}
+          </Link>
+        </>
+      )}
     </div>
   );
 };
