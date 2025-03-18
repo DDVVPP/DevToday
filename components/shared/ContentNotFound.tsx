569a28298bc233d1449c6951c2260f2178d03846
DDVVPP <darshinvp@gmail.com>
2025-03-18 10:00:16 -0700

    refactor: remove duplicate code in ContentNotFound component

--- components/shared/ContentNotFound.tsx
+++ components/shared/ContentNotFound.tsx
@@ -12,32 +12,24 @@ const ContentNotFound = ({
 
   return (
     <div className="paragraph-1-medium m-20 flex flex-col justify-center gap-y-6 text-dark-700 dark:text-white-200">
-      {isPlural ? (
-        <>
-          <h1 className="text-center">{contentCategory}s not found</h1>
-          <Link
-            className="rounded bg-primary-500 px-4 py-2 text-white-200 hover:opacity-70 hover:duration-300"
-            href={`/${pluralLowerCaseContentType}`}
-          >
-            Click here to view all {pluralLowerCaseContentType}
-          </Link>
-          <Link
-            className="rounded bg-primary-500 px-4 py-2 text-center text-white-200 hover:opacity-70 hover:duration-300"
-            href={`/${pluralLowerCaseContentType}/create-post`}
-          >
-            Click here to create {contentCategory.toLowerCase()}
-          </Link>
-        </>
-      ) : (
-        <>
-          <h1 className="text-center">{contentCategory} not found</h1>
-          <Link
-            className="rounded bg-primary-500 px-4 py-2 text-white-200 hover:opacity-70 hover:duration-300"
-            href={`/${pluralLowerCaseContentType}`}
-          >
-            Click here to view all {pluralLowerCaseContentType}
-          </Link>
-        </>
+      <h1 className="text-center">
+        {contentCategory}
+        {isPlural && "s"} not found
+      </h1>
+      <Link
+        className="rounded bg-primary-500 px-4 py-2 text-white-200 hover:opacity-70 hover:duration-300"
+        href={`/${pluralLowerCaseContentType}`}
+      >
+        Click here to view all {pluralLowerCaseContentType}
+      </Link>
+
+      {isPlural && (
+        <Link
+          className="rounded bg-primary-500 px-4 py-2 text-center text-white-200 hover:opacity-70 hover:duration-300"
+          href={`/${pluralLowerCaseContentType}/create-post`}
+        >
+          Click here to create {contentCategory.toLowerCase()}
+        </Link>
       )}
     </div>
   );
